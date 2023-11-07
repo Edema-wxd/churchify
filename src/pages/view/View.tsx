fab0a1fa403234da5bfae8deaf2af7b0fa52fda6
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-11-07 21:58:53 +0100

    added the video player

--- src/pages/view/View.tsx
+++ src/pages/view/View.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
 
 function View() {
   const params = useParams<string>();
-  return <div>View{params.id}</div>;
+  return (
+    <div>
+      View{params.id}
+      <br />
+      <ReactPlayer
+        width={320}
+        height={240}
+        controls={false}
+        url="https://www.youtube.com/watch?v=QyFcl_Fba-k"
+      />
+    </div>
+  );
 }
 
 export default View;
